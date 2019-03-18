5ce4eb8fb67f036017b35d0f52d839096c426ddc
LutTianTian <2833038499@qq.com>
2019-03-18 11:04:08 +0800

    路甜甜新建Test类

---
+++ src/main/Java/com/huayu/Test.java
@@ -0,0 +1,14 @@
+package com.huayu;
+
+/**
+ * @Date : 2019/3/18 10:59
+ * @Auther:asus-pc
+ * @Description:
+ */
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("新建test类");
+    	}
+}
